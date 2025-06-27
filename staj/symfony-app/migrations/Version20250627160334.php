ed6a194b9778ee47e75f169b192a1d20826fa27b
harunkaraca <karacalar500@gmail.com>
2025-06-27 19:19:39 +0300

    veritabanı oluşturuldu kayıt formları oluşturuldu

--- staj/symfony-app/migrations/Version20250627160334.php
+++ staj/symfony-app/migrations/Version20250627160334.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250626131309 extends AbstractMigration
+final class Version20250627160334 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,22 +20,12 @@ final class Version20250626131309 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql(<<<'SQL'
-            CREATE TABLE "user" (id SERIAL NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, PRIMARY KEY(id))
-        SQL);
-        $this->addSql(<<<'SQL'
-            CREATE UNIQUE INDEX UNIQ_IDENTIFIER_EMAIL ON "user" (email)
-        SQL);
+
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql(<<<'SQL'
-            CREATE SCHEMA public
-        SQL);
-        $this->addSql(<<<'SQL'
-            DROP TABLE "user"
-        SQL);
+        $this->addSql('CREATE SCHEMA public');
     }
 }
